3f86014db1f000e6205bc46328d43682872f4653
SiphesihleMazibuko <amazibuko763@gmail.com>
2024-07-31 23:12:35 +0200

    Updated the code

--- Password.py
+++ Password.py
@@ -5,9 +5,27 @@ upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "1234567890"
 symbols = "`!@#$%^&*()_+=-~{}[]';/?><,"
 password = " "
-combination = lowerCase + upperCase + numbers + symbols
-length = int(input('Enter the length of your password: '))
 
-for i in range(length):
-    password += random.choice(combination)
-print(password)
+letters = int(input('Enter the letters would you like in your password?\n'))
+symbols_ = int(input('How many symbols would you like?\n'))
+nums = int(input('How many numbers would you like?\n'))
+
+password_characters = []
+
+for i in range(letters):
+    password_characters.append(random.choice(lowerCase + upperCase))
+
+for i in range(symbols_):
+    password_characters.append(random.choice(symbols))
+
+for i in range(nums):
+    password_characters.append(random.choice(numbers))
+
+random.shuffle(password_characters)
+
+password = ''.join(password_characters)
+
+print('Here is your password:',password)
+
+
+
